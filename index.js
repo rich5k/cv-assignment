bf7e66f0b8f691e01e889ed526d199d11ea36d6e
rich5k <richardanatsui@gmail.com>
2020-09-20 20:28:01 +0100

    started working on local storage

---
+++ index.js
@@ -0,0 +1,45 @@
+const storeFname = document.querySelector("#fname");
+const storeMname = document.querySelector("#mname");
+const storeLname = document.querySelector("#lname");
+const nameCheck = document.querySelector(".nameCheck");
+var fullName= "";
+var fname="";
+var mname="";
+var lname="";
+storeFname.addEventListener('input', letter =>{
+    fname= letter.target.value;
+    if(mname.length!=0){
+        fullName= fname+ " "+ mname;
+        nameCheck.textContent=fullName;
+    }
+    else{
+        fullName=fname;
+        nameCheck.textContent=fullName;
+
+    }
+    console.log(fullName);
+})
+
+storeMname.addEventListener('input', letter =>{
+    mname=letter.target.value;
+
+    if(mname[mname.length-1]==fullName[fullName.length-1]){
+        fullName=fullName+ mname[mname.length-1];
+        nameCheck.textContent=fullName;
+        
+    }
+    else{
+        fullName=fname+ " "+ mname;
+        nameCheck.textContent=fullName;
+
+    }
+    console.log(fullName);
+})
+// fullName= fullName+newName;
+
+storeLname.addEventListener('input', letter =>{
+    lname=letter.target.value;
+    nameCheck.textContent=lname+" "+fullName+" "+mname;
+    console.log(fullName);
+})
+fullName= fname+mname+lname;
