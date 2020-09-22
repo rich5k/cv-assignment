2773d6308fab9643948cfdbff5452ee55af632dc
rich5k <richardanatsui@gmail.com>
2020-09-22 19:02:15 +0100

    commented the files

--- cv.js
+++ cv.js
@@ -1,8 +1,12 @@
 const name= document.querySelector(".name");
+// replaces the name in the bio section with full name in localstorage
 name.innerHTML=localStorage.getItem("textvalue");
 const address= document.querySelector(".address");
+// replaces the address in the bio section with address in localstorage
 address.innerHTML=localStorage.getItem("address");
 const email= document.querySelector(".email");
+// replaces the email in the bio section with email in localstorage
 email.innerHTML=localStorage.getItem("femail");
 const number= document.querySelector(".number");
+// replaces the phone number in the bio section with phone number in localstorage
 number.innerHTML=localStorage.getItem("phone");
--- index.js
+++ index.js
@@ -8,27 +8,28 @@ var fname="";
 var mname="";
 var lname="";
 
-
+//takes the first name and capitalizes
 storeFname.addEventListener('input', letter =>{
     fname= letter.target.value.toUpperCase();
 })
-
+//takes the middle name and capitalizes
 storeMname.addEventListener('input', letter =>{
     mname=letter.target.value.toUpperCase();
 })
 
-
+//takes the last name and capitalizes
 storeLname.addEventListener('input', letter =>{
     lname=letter.target.value.toUpperCase();
 })
 
-
+//takes the address 
 const storeAddress= document.querySelector("#address");
 var address="";
 storeAddress.addEventListener('input', letter =>{
     address= letter.target.value;
     
 })
+//takes the phone number 
 const storePhone= document.querySelector("#phoneNumber");
 var phoneNum="";
 storePhone.addEventListener('input', letter =>{
@@ -36,7 +37,7 @@ storePhone.addEventListener('input', letter =>{
     
     console.log(phoneNum);
 })
-
+//takes the email
 const storeEmail= document.querySelector("#email");
 var email="";
 storeEmail.addEventListener('input', letter =>{
@@ -45,19 +46,25 @@ storeEmail.addEventListener('input', letter =>{
     console.log(email);
 })
 console.log(email);
+
+// stores the first, middle and last names in localstorage
 function sendnames(){
     fullName= lname+" "+fname+" "+mname;
     localStorage.setItem("textvalue",fullName);
 }
+//stores address in localstorage
 function sendaddress(){
     localStorage.setItem("address",address);
 }
+//stores email in localstorage
 function sendemail(){
     localStorage.setItem("femail",email);
 }
+//stores phone in localstorage
 function sendphone(){
     localStorage.setItem("phone",phoneNum);
 }
+//sends to data to next page 
 function sendvalues(){
     sendnames();
     sendemail();
@@ -65,7 +72,7 @@ function sendvalues(){
     sendaddress();
     return false;
 }
-
+//validates first name
 function validateFname(e){
     if (fname == "") {
         alert("First name must be filled out");
@@ -76,7 +83,7 @@ function validateFname(e){
         return true;
     }
 }
-
+//validates middle name
 function validateMname(e){
     if (mname == "") {
         alert("Middle name must be filled out");
@@ -88,6 +95,7 @@ function validateMname(e){
     }
       
 }
+//validates last name
 function validateLname(e){
     if (lname == "") {
         alert("Last name must be filled out");
@@ -99,6 +107,7 @@ function validateLname(e){
       }
       
 }
+//validates address
 function validateAddress(e){
     if (address == "") {
         console.log(address);
@@ -111,6 +120,7 @@ function validateAddress(e){
       }
       
 }
+//validates phone number
 function validatePhone(e){
     if (phoneNum == "") {
         alert("Phone number must be filled out");
@@ -132,7 +142,7 @@ function validatePhone(e){
       }
       
 }
-
+//validates the whole form
 function formValid(e) {
     if(!(validateFname(e)&& validateMname(e) && validateLname(e)&&validateAddress(e)&&validatePhone(e))){
         e.preventDefault();
